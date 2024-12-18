6a5ba628414745a172af4af74a2760361f2ab064
Kevin <kevin.johansson5@elev.ga.ntig.se>
2024-12-18 15:09:04 +0100

    GODJUL

--- js/Particle.js
+++ js/Particle.js
@@ -2,16 +2,16 @@ class Particle {
   constructor(x, y, color) {
     this.x = x;
     this.y = y;
-    this.dy = (1 + Math.random() * 3) * speed / 5;
-    this.dx = (-1 + Math.random() * 2) * speed / 5;
-    this.color = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${Math.random()})`;
-    this.size = 2 + Math.floor(Math.random() * 2);
+    this.dy = (1 + Math.random() * 10) * speed / 3;
+    this.dx = (-5 + Math.random(100) * 10) * speed / 3;
+    this.color = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${Math.random(6)})`;
+    this.size = 20 + Math.floor(Math.random() * 2);
     this.toDelete = false;
   }
 
   draw() {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, pi2, false);
+    ctx.arc(this.x, this.y, this.size, 7, pi2, false);
     ctx.fillStyle = this.color;
     ctx.fill();
   }
